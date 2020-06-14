953d39eef144483ced822f52a80dab15d2066301
Wiktorcepil <64475107+Wiktorcepil@users.noreply.github.com>
2020-06-14 21:55:05 +0200

    Update Create_Character.h

--- test3/WH2_Character_Generator/Create_Character.h
+++ test3/WH2_Character_Generator/Create_Character.h
@@ -1,14 +1,13 @@
 #pragma once 
-
 class Create_Character
 {
+
 	//konstruktor Create_Charakter							inte zamiast int z oczywistych wzgledow technicznych
-public: Create_Character(int ww, int us, int k, int odp, int zr, int inte, int sw, int ogd, int a, int zyw, int s, int wt, int sz, int mag, int po, int pp);
-		
-		int raceid=1; //indetygikator rasy. 1 human, 2 elf, 3 halfling, 4 dwarf
+public: 
+		Create_Character(int ww, int us, int k, int odp, int zr, int inte, int sw, int ogd, int a, int zyw, int s, int wt, int sz, int mag, int po, int pp);
+		int raceid=1; //indetyfikator rasy. 1 human, 2 elf, 3 halfling, 4 dwarf PRZYKLADOWE USTAWIENIE NA 1 => CZLOWIEK 
 		int stat_roll; // do losowania statystyk typu PP
+	
 private: int stat_numb;
-		
 
 };
-
