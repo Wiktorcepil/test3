c0b662e5d154e65b2f2fc616eeb1856a63d164df
Wiktorcepil <64475107+Wiktorcepil@users.noreply.github.com>
2020-06-17 22:35:06 +0200

    added "tutorial" to main.cpp

--- test3/WH2_Character_Generator/main.cpp
+++ test3/WH2_Character_Generator/main.cpp
@@ -4,6 +4,7 @@
 
 int main()
 {
+	std::cout << "wcisnij 1 by stworzyc statystyki dla czlowieka \n2 by stworzyc statystyki dla elfa \nq by wyjsc z generatora" << std::endl;
 	Controller();
 	return 0;
 }
